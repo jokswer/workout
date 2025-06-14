4decc1e2d44da541fd7ae8dafef58b274a307c78
jokswer <jokswer@gmail.com>
2025-06-14 12:57:12 +0700

    fix web init

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,28 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
 import { isValid } from '@telegram-apps/init-data-node';
+import { ZodValidationPipe } from 'src/utils/zodValidation.pipe';
+import { WebAppInitDto, WebAppInitSchema } from './schemas/webAppInit.schema';
 import { AuthService } from './auth.service';
-import { WebAppInitDto } from './schemas/webAppInit.schema';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/web-app-init')
+  @UsePipes(new ZodValidationPipe(WebAppInitSchema))
   public webAppInit(@Body() body: WebAppInitDto) {
     const { initData } = body;
 
-    const isInitDataValid = isValid(initData, process.env.BOT_TOKEN ?? '');
+    const isInitDataValid = isValid(initData, process.env.BOT_TOKEN!);
 
     if (!isInitDataValid) {
-      throw new BadRequestException('AUTH__INVALID_INITDATA');
+      throw new BadRequestException('Invalid data');
     }
 
     return this.authService.webAppInit(initData);
